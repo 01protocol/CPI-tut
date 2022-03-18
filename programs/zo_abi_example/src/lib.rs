592c1010765797f3359b1f9c1577fc5d0b05bd11
zoddn <nosferatuzodd000@gmail.com>
2022-03-18 12:00:50 -0400

    added more stuff

--- programs/zo_abi_example/src/lib.rs
+++ programs/zo_abi_example/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Token, TokenAccount};
-use zo::{self, program::ZoAbi as Zo, State, Margin, Control, Cache};
+use zo::{self, program::ZoAbi as Zo, Cache, Control, Margin, OrderType, State};
 
 declare_id!("Eg8fBwr5N3P9HTUZsKJ5LZP3jVRgBJcrnvAcY35G5rTw");
 
@@ -63,6 +63,90 @@ pub mod zo_abi_example {
         zo::cpi::withdraw(cpi_cx, false, amount)?;
         Ok(())
     }
+
+    pub fn do_create_perp_open_orders(cx: Context<DoCreatePerpOpenOrders>) -> ProgramResult {
+        msg!("calling create_perp_open_orders.rs through CPI");
+
+        let cpi_program = cx.accounts.zo_program.to_account_info();
+        let cpi_accounts = zo::cpi::accounts::CreatePerpOpenOrders {
+            state: cx.accounts.zo_program_state.to_account_info(),
+            state_signer: cx.accounts.state_signer.to_account_info(),
+            authority: cx.accounts.authority.to_account_info(),
+            payer: cx.accounts.authority.to_account_info(),
+            margin: cx.accounts.zo_program_margin.to_account_info(),
+            control: cx.accounts.control.to_account_info(),
+            open_orders: cx.accounts.open_orders.to_account_info(),
+            dex_market: cx.accounts.dex_market.to_account_info(),
+            dex_program: cx.accounts.dex_program.to_account_info(),
+            rent: cx.accounts.rent.to_account_info(),
+            system_program: cx.accounts.system_program.to_account_info(),
+        };
+        let cpi_cx = CpiContext::new(cpi_program, cpi_accounts);
+        zo::cpi::create_perp_open_orders(cpi_cx)?;
+        Ok(())
+    }
+
+    pub fn do_place_perp_order(
+        cx: Context<DoPlacePerpOrder>,
+        is_long: bool,
+        limit_price: u64,
+        max_base_quantity: u64,
+        max_quote_quantity: u64,
+        client_id: u64,
+        limit: u16,
+    ) -> ProgramResult {
+        msg!("calling place_perp_order.rs through CPI");
+
+        let cpi_program = cx.accounts.zo_program.to_account_info();
+        let cpi_accounts = zo::cpi::accounts::PlacePerpOrder {
+            state: cx.accounts.zo_program_state.to_account_info(),
+            state_signer: cx.accounts.state_signer.to_account_info(),
+            cache: cx.accounts.cache.to_account_info(),
+            authority: cx.accounts.authority.to_account_info(),
+            margin: cx.accounts.zo_program_margin.to_account_info(),
+            control: cx.accounts.control.to_account_info(),
+            open_orders: cx.accounts.open_orders.to_account_info(),
+            dex_market: cx.accounts.dex_market.to_account_info(),
+            req_q: cx.accounts.req_q.to_account_info(),
+            event_q: cx.accounts.event_q.to_account_info(),
+            market_bids: cx.accounts.market_bids.to_account_info(),
+            market_asks: cx.accounts.market_asks.to_account_info(),
+            dex_program: cx.accounts.dex_program.to_account_info(),
+            rent: cx.accounts.rent.to_account_info(),
+        };
+        let cpi_cx = CpiContext::new(cpi_program, cpi_accounts);
+        zo::cpi::place_perp_order(
+            cpi_cx,
+            is_long,
+            limit_price,
+            max_base_quantity,
+            max_quote_quantity,
+            OrderType::Limit,
+            limit,
+            client_id,
+        )?;
+        Ok(())
+    }
+
+    pub fn do_settle_funds(cx: Context<DoSettleFunds>) -> ProgramResult {
+        msg!("calling settle_funds.rs through CPI");
+
+        let cpi_program = cx.accounts.zo_program.to_account_info();
+        let cpi_accounts = zo::cpi::accounts::SettleFunds {
+            authority: cx.accounts.authority.to_account_info(),
+            state: cx.accounts.zo_program_state.to_account_info(),
+            state_signer: cx.accounts.state_signer.to_account_info(),
+            cache: cx.accounts.cache.to_account_info(),
+            margin: cx.accounts.zo_program_margin.to_account_info(),
+            control: cx.accounts.control.to_account_info(),
+            open_orders: cx.accounts.open_orders.to_account_info(),
+            dex_market: cx.accounts.dex_market.to_account_info(),
+            dex_program: cx.accounts.dex_program.to_account_info(),
+        };
+        let cpi_cx = CpiContext::new(cpi_program, cpi_accounts);
+        zo::cpi::settle_funds(cpi_cx)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -73,10 +157,10 @@ pub struct DoCreateMargin<'info> {
     #[account(mut)]
     pub zo_program_state: AccountLoader<'info, State>,
     #[account(mut)]
-    pub zo_program_margin: AccountLoader<'info, Margin>,
+    pub zo_program_margin: UncheckedAccount<'info>,
     pub zo_program: Program<'info, Zo>,
     #[account(mut)]
-    pub control: AccountLoader<'info, Control>,
+    pub control: UncheckedAccount<'info>,
     pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
 }
@@ -127,4 +211,102 @@ pub struct DoWithdraw<'info> {
     #[account(mut)]
     pub zo_program_vault: Account<'info, TokenAccount>,
     pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct DoCreatePerpOpenOrders<'info> {
+    pub zo_program: Program<'info, Zo>,
+    pub zo_program_state: AccountLoader<'info, State>,
+    // this will be checked in 01 program
+    // seeds = [zo_program_state.key().as_ref()],
+    // bump = zo_program_state.load()?.signer_nonce
+    #[account(mut)]
+    pub state_signer: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    // this will be checked in 01 program
+    // seeds = [authority.key.as_ref(), zo_program_state.key().as_ref(), b"marginv1".as_ref()],
+    // bump = zo_program_margin.load()?.nonce
+    pub zo_program_margin: AccountLoader<'info, Margin>,
+    #[account(
+        mut,
+        address = zo_program_margin.load()?.control
+    )]
+    pub control: AccountLoader<'info, Control>,
+    #[account(mut)]
+    pub open_orders: UncheckedAccount<'info>,
+    //owner should be serum dex id
+    #[account(mut)]
+    pub dex_market: UncheckedAccount<'info>,
+    //address should be serum dex id
+    pub dex_program: UncheckedAccount<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DoPlacePerpOrder<'info> {
+    pub zo_program: Program<'info, Zo>,
+    pub zo_program_state: AccountLoader<'info, State>,
+    #[account(mut)]
+    // this will be checked in 01 program
+    // seeds = [zo_program_state.key().as_ref()],
+    // bump = zo_program_state.load()?.signer_nonce
+    pub state_signer: UncheckedAccount<'info>,
+    #[account(mut, address = zo_program_state.load()?.cache)]
+    pub cache: AccountLoader<'info, Cache>,
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    // this will be checked in 01 program
+    // seeds = [authority.key.as_ref(), zo_program_state.key().as_ref(), b"marginv1".as_ref()],
+    // bump = zo_program_margin.load()?.nonce
+    pub zo_program_margin: AccountLoader<'info, Margin>,
+    #[account(
+        mut,
+        address = zo_program_margin.load()?.control
+    )]
+    pub control: AccountLoader<'info, Control>,
+    #[account(mut)]
+    pub open_orders: UncheckedAccount<'info>,
+    //owner should be serum dex id
+    #[account(mut)]
+    pub dex_market: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub req_q: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub event_q: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub market_bids: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub market_asks: UncheckedAccount<'info>,
+    //address should be serum dex id
+    pub dex_program: UncheckedAccount<'info>,
+    pub rent: Sysvar<'info, Rent>,
+}
+#[derive(Accounts)]
+pub struct DoSettleFunds<'info> {
+    pub zo_program: Program<'info, Zo>,
+    pub authority: Signer<'info>,
+    pub zo_program_state: AccountLoader<'info, State>,
+    #[account(mut)]
+    // this will be checked in 01 program
+    // seeds = [zo_program_state.key().as_ref()],
+    // bump = zo_program_state.load()?.signer_nonce
+    pub state_signer: UncheckedAccount<'info>,
+    #[account(mut, address = zo_program_state.load()?.cache)]
+    pub cache: AccountLoader<'info, Cache>,
+    #[account(mut)]
+    // this will be checked in 01 program
+    // seeds = [authority.key.as_ref(), zo_program_state.key().as_ref(), b"marginv1".as_ref()],
+    // bump = zo_program_margin.load()?.nonce
+    pub zo_program_margin: AccountLoader<'info, Margin>,
+    #[account(mut, address = zo_program_margin.load()?.control)]
+    pub control: AccountLoader<'info, Control>,
+    #[account(mut)]
+    pub open_orders: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub dex_market: UncheckedAccount<'info>,
+    //address should be serum dex id
+    pub dex_program: UncheckedAccount<'info>,
 }
--- tests/zo_abi_example.ts
+++ tests/zo_abi_example.ts
@@ -12,14 +12,30 @@ import {
   CONTROL_ACCOUNT_SIZE,
   createProgram,
   Margin,
+  ZO_DEX_DEVNET_PROGRAM_ID,
   State,
   USDC_DECIMALS,
+  baseNumberToLots,
+  priceNumberToLots,
+  ZoOpenOrders
 } from "@zero_one/client";
 import * as splToken from "@solana/spl-token";
 import assert from "assert";
 
 import idl = require("../target/idl/zo_abi_example.json");
 import { TOKEN_PROGRAM_ID } from "@project-serum/serum/lib/token-instructions";
+
+import fetch from "node-fetch";
+
+const SOL_DECIMALS = 9;
+const SOL_OG_FEE = 10; // bps
+const SOL_LOT_SIZE = new BN(100_000_000);
+const SOL_SQ_LOT_SIZE = new BN(10_000);
+const USDC_SOL_LOT_SIZE = new BN(100);
+const SOL_BASE_IMF = new BN(100);
+const SOL_LIQ_FEE = new BN(20);
+const SOL_SQUARE_SYMBOL = "SOL-SQUARE";
+export const STARTING_FUNDING_INDEX = 10 ** 9;
 
 const SETTINGS = {
   connection: "https://api.devnet.solana.com",
@@ -51,8 +67,8 @@ describe("zo_abi_example", () => {
       // @ts-ignore
       new anchor.Wallet(userAcc),
       {
-        preflightCommitment: "confirmed",
-        commitment: "confirmed",
+        preflightCommitment: "finalized",
+        commitment: "finalized",
       }
     );
 
@@ -61,11 +77,13 @@ describe("zo_abi_example", () => {
     ts.program = new anchor.Program(idl as Idl, SETTINGS.programPid, ts.user);
 
     console.log("user wallet: ", ts.user.wallet.publicKey.toString());
+
+    //@ts-ignore
     ts.userUSDC = await splToken.getOrCreateAssociatedTokenAccount(
       ts.user.connection,
-      ts.user.wallet,
+      ts.user.wallet.payer,
       SETTINGS.usdcMint,
-      ts.user.wallet.publicKey
+      ts.user.wallet.publicKey,
     );
 
     console.log("user token account", ts.userUSDC.address.toString());
@@ -78,7 +96,7 @@ describe("zo_abi_example", () => {
     console.log("zo program USDC vault: ", ts.stateUSDCVault.toString());
   });
 
-  it("Allows users to create margin from 01_exchange", async () => {
+  xit("Allows users to create margin from 01_exchange", async () => {
     //creating accounts needed for CreateMargin call
     const [[key, nonce], control, controlLamports] = await Promise.all([
       PublicKey.findProgramAddress(
@@ -87,7 +105,7 @@ describe("zo_abi_example", () => {
           ts.state.pubkey.toBuffer(),
           Buffer.from("marginv1"),
         ],
-        ts.zoProgram.programId
+        SETTINGS.zoPid
       ),
       Keypair.generate(),
       ts.program.provider.connection.getMinimumBalanceForRentExemption(
@@ -115,7 +133,7 @@ describe("zo_abi_example", () => {
             newAccountPubkey: control.publicKey,
             lamports: controlLamports,
             space: CONTROL_ACCOUNT_SIZE,
-            programId: ts.zoProgram.programId,
+            programId: SETTINGS.zoPid,
           }),
         ],
         signers: [control],
@@ -124,7 +142,20 @@ describe("zo_abi_example", () => {
     }
   });
 
-  it("allows users to fetch margin account data", async () => {
+  xit("funds the token account with the faucet", async () => {
+
+    //making a post request to call faucet for USDC
+    const response = await fetch('https://devnet-faucet.01.xyz?'
+    +'owner='+ts.user.wallet.publicKey.toString()
+    +'&mint=7UT1javY6X1M9R2UrPGrwcZ78SX3huaXyETff5hm5YdX'
+    +'&amount=1500000000'
+    , {method: 'POST'});
+    
+    if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+
+  });
+
+  it("Allows users to fetch margin account data", async () => {
     //fetching margin data
     ts.userMargin = await Margin.load(ts.zoProgram, ts.state);
 
@@ -143,7 +174,7 @@ describe("zo_abi_example", () => {
     assert.ok(ts.userMargin.data.control.equals(ts.userControl.pubkey));
   });
 
-  it("allows users to make a deposit USDC", async () => {
+  xit("Allows users to make a deposit USDC", async () => {
     const depositAmount = new BN(40 * 10 ** USDC_DECIMALS);
     console.log("depositing amount: ", depositAmount.toString());
 
@@ -185,7 +216,7 @@ describe("zo_abi_example", () => {
 
   });
 
-  it("allows users to make a withdraw USDC", async () => {
+  xit("Allows users to make a withdraw USDC", async () => {
     const withdrawAmount = new BN(20 * 10 ** USDC_DECIMALS);
     
     console.log("withdrawing amount: ", withdrawAmount.toString());
@@ -228,4 +259,158 @@ describe("zo_abi_example", () => {
     assert.equal(fetchVaultBalanceAfter.value.amount-fetchVaultBalanceBefore.value.amount, -withdrawAmount.toNumber());
 
   });
+
+  xit("Allows users to make an open order account", async () => {
+
+    const dexMarket = ts.state.getMarketKeyBySymbol("SOL-PERP");
+    const [ookey, _] = await PublicKey.findProgramAddress(
+      [ts.userMargin.data.control.toBuffer(), dexMarket.toBuffer()],
+      ZO_DEX_DEVNET_PROGRAM_ID
+    );
+
+    if (await ts.program.provider.connection.getAccountInfo(ookey)) {
+      console.log("open order account already exists");
+    } else {
+      const tx = await ts.program.rpc.doCreatePerpOpenOrders({
+        accounts: {
+          zoProgram: SETTINGS.zoPid,
+          zoProgramState: ts.state.pubkey,
+          stateSigner: ts.userMargin.state.signer,
+          authority: ts.user.wallet.publicKey,
+          zoProgramMargin: ts.userMargin.pubkey,
+          control: ts.userMargin.control.pubkey,
+          openOrders: ookey,
+          dexMarket: ts.state.getMarketKeyBySymbol("SOL-PERP"),
+          dexProgram: ZO_DEX_DEVNET_PROGRAM_ID,
+          rent: SYSVAR_RENT_PUBKEY,
+          systemProgram: SystemProgram.programId,
+        },
+      });
+      await ts.program.provider.connection.confirmTransaction(tx, "finalized");
+    }
+
+    console.log("ookey: ", ookey.toString());
+
+  });
+
+  describe("Placing perp orders", async() => {
+
+    const userClientId = new BN(1);
+
+    xit("Allows users to place a long sol perp order", async() => {
+      const price = 90; //in big USD
+      const size = 1; //in bif SOL
+  
+      const isLong = true;
+      const limitPrice: BN = priceNumberToLots(
+        price,
+        SOL_DECIMALS,
+        SOL_LOT_SIZE,
+        USDC_DECIMALS,
+        USDC_SOL_LOT_SIZE,
+      );
+  
+      const maxBaseQty: BN = baseNumberToLots(size, SOL_DECIMALS, SOL_LOT_SIZE);
+      const maxQuoteQty: BN = limitPrice.mul(maxBaseQty).mul(USDC_SOL_LOT_SIZE);
+      console.log("maxquote actual: ", maxQuoteQty.toNumber());
+
+      const market = await ts.userMargin.state.getMarketBySymbol("SOL-PERP");
+      const oo = await ts.userMargin.getOpenOrdersInfoBySymbol("SOL-PERP");
+
+      ts.userMargin.control.refresh();
+      console.log("ookey: ", ts.userMargin.control.data.openOrdersAgg[1].key.toString());
+
+      const tx = await ts.program.rpc.doPlacePerpOrder(
+        isLong,
+        limitPrice,
+        maxBaseQty,
+        maxQuoteQty,
+        userClientId,
+        10,
+        {
+          accounts: {
+            zoProgram: SETTINGS.zoPid,
+            zoProgramState: ts.state.pubkey,
+            stateSigner: ts.userMargin.state.signer,
+            cache: ts.state.cache.pubkey,
+            authority: ts.user.wallet.publicKey,
+            zoProgramMargin: ts.userMargin.pubkey,
+            control: ts.userMargin.control.pubkey,
+            openOrders: oo.key,
+            dexMarket: market.address,
+            reqQ: market.requestQueueAddress,
+            eventQ: market.eventQueueAddress,
+            marketBids: market.bidsAddress,
+            marketAsks: market.asksAddress,
+            dexProgram: ZO_DEX_DEVNET_PROGRAM_ID,
+            rent: SYSVAR_RENT_PUBKEY,
+          }
+        }
+      );
+      await ts.program.provider.connection.confirmTransaction(tx, "finalized");
+
+      //console.log(ts.userMargin.control);
+      ts.userOo = await ZoOpenOrders.load(
+        ts.program.provider.connection,
+        ts.userMargin.control.data.openOrdersAgg[1].key,
+        ZO_DEX_DEVNET_PROGRAM_ID,
+      );
+      //console.log(ts.userOo);
+      assert.equal(ts.userOo.quoteTokenTotal.toNumber(), 0);
+      console.log("quoteTokenFree: ", ts.userOo.quoteTokenFree.toNumber());
+      // assert.equal(
+      //   ts.userOo.quoteTokenFree.toNumber(),
+      //   (-maxQuoteQty.toNumber()) * 2,
+      // );
+      assert.equal(ts.userOo.baseTokenTotal.toNumber(), 0);
+      assert.equal(ts.userOo.baseTokenFree.toNumber(), 0);
+      assert.equal(ts.userOo.realizedPnl.toNumber(), 0);
+      console.log("fundingIndex: ",ts.userOo.fundingIndex.toNumber());
+      //assert.equal(ts.userOo.fundingIndex.toNumber(), STARTING_FUNDING_INDEX);
+      console.log("coinOnBids: ", ts.userOo.coinOnBids.toNumber());
+      // assert.equal(
+      //   ts.userOo.coinOnBids.toNumber(),
+      //   maxBaseQty.toNumber() * SOL_LOT_SIZE.toNumber() * 2,
+      // );
+      assert.equal(ts.userOo.coinOnAsks.toNumber(), 0);
+
+    });
+    
+    it("Allows users to settle funds", async() => {
+      const market = await ts.userMargin.state.getMarketBySymbol("SOL-PERP");
+      const oo = await ts.userMargin.getOpenOrdersInfoBySymbol("SOL-PERP");
+
+      await ts.userMargin.refresh();
+      console.log("user Margin raw col USDC balance before settle funds: ", ts.userMargin.data.rawCollateral[0].toNumber());
+      console.log("user Margin actual col USDC balance before settle funds: ", ts.userMargin.data.actualCollateral[0].n.toNumber());
+      console.log("user Margin USDC balance before settle funds: ", ts.userMargin.balances.USDC.n.toString());
+
+      console.log("position size:", ts.userMargin.positions[0].coins.number);
+
+      const tx = await ts.program.rpc.doSettleFunds({
+        accounts: {
+          zoProgram: SETTINGS.zoPid,
+          authority: ts.user.wallet.publicKey,
+          zoProgramState: ts.state.pubkey,
+          stateSigner: ts.userMargin.state.signer,
+          cache: ts.state.cache.pubkey,
+          zoProgramMargin: ts.userMargin.pubkey,
+          control: ts.userMargin.control.pubkey,
+          openOrders: oo.key,
+          dexMarket: market.address,
+          dexProgram: ZO_DEX_DEVNET_PROGRAM_ID,
+        },
+      });
+
+      await ts.program.provider.connection.confirmTransaction(tx, "finalized");
+
+      await ts.userMargin.refresh();
+      console.log("user Margin raw col USDC balance after settle funds: ", ts.userMargin.data.rawCollateral[0].toNumber());
+      console.log("user Margin actual col USDC balance after settle funds: ", ts.userMargin.data.actualCollateral[0].n.toNumber());
+      console.log("user Margin USDC balance after settle funds: ", ts.userMargin.balances.USDC.n.toString());
+
+    });
+
+  })
+
 });
